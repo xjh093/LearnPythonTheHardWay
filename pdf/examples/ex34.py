53454e2fad3fbe21a71d22580a9dca517728a31c
HaoCold <xjh093@126.com>
2018-06-15 13:52:57 +0800

    Add files via upload

---
+++ pdf/examples/ex34.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+# p104
+
+animals = ['bear','python','peacock','kangaroo','whale','platypus']
+
+print("The animal at 1 is %s",animals[0])
+print("The third animal is %s",animals[2])
+print("The first animal is %s",animals[0])
+print("The animal at 3 is %s",animals[2])
+print("The fifth animal is %s",animals[4])
+print("The animal at 2 is %s",animals[1])
+print("The sixth animal is %s",animals[5])
+print("the animal at 4 is %s",animals[3])
