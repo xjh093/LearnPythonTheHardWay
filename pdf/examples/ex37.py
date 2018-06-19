eec7a624e9ffeb03d041538c23d937ebae0827d7
HaoCold <xjh093@126.com>
2018-06-19 12:52:33 +0800

    Add files via upload

---
+++ pdf/examples/ex37.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python3
+# p111
+
+# 关键字
+
+# exec  
+# 将字符串作为Python代码执行 
+# exec "print('hello')"
+
+# global
+# 定义一个全局变量
+
+# is
+# 类似 == ，判断相等
+
+# pass
+# 该代码块为空
+
+# raise
+# 代码出错时，抛出一个异常
+
+# yield 
+# 暂停，返回给调用者
+
+
